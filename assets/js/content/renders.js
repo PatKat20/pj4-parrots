396094c9ad1c1afa6c43352ff642fec945d665f2
PatKatXD <100157955+PatKat20@users.noreply.github.com>
2023-01-12 13:49:02 -0300

    Adiciona o JS e faz a distribuicao de cartas

---
+++ assets/js/content/renders.js
@@ -0,0 +1,25 @@
+import { shuffleArray } from "../utils/utils.js"
+import { insertEventOnClick, renderCards, cardsList } from "../events/oncardEvents.js";
+let numberOfPlays = 0;
+
+function start(){
+    do{
+        numberOfPlays = prompt("Quantas cartas deseja jogar? (Somente números pares de 4 a 14)")
+    } while(numberOfPlays < 4 || numberOfPlays > 14 || numberOfPlays % 2 !== 0)
+
+    renderCards(numberOfPlays)
+    shuffleCardsAndInsertIntoHtml()
+    insertEventOnClick()
+}
+
+function shuffleCardsAndInsertIntoHtml(){
+    const lista = document.getElementById("cardsList")
+    cardsList.push(...cardsList)
+    const cardsListShufled = shuffleArray(cardsList)
+
+    cardsListShufled.forEach(card =>{
+        lista.innerHTML += card
+    })
+}
+
+export { start , numberOfPlays }
